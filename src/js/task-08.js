6779e5de9bfb7a883f8e9cbf9d1412ce403df721
Lytvynenko-Oleksandr <dp180889los@gmail.com>
2023-05-08 18:14:21 +0300

    add tasks 8 and 9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const emailInputEl = formEl.elements.email;
+    const passwordInputEl = formEl.elements.password;
+
+    console.log(emailInputEl, passwordInputEl);
+
+    if (emailInputEl.value === "" || passwordInputEl.value === "") { 
+        alert('Заполнены не все обязательные поля');
+        return;
+    };
+
+    const formData = {
+    email: emailInputEl.value,
+    password: passwordInputEl.value,
+    };
+    
+    console.log(formData);
+
+    formEl.reset();
+});
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,12 @@
+const changeColorBtnEl = document.querySelector('.change-color');
+const nameColorForBodyEl = document.querySelector('.color');
+
+changeColorBtnEl.addEventListener('click', () => { 
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  nameColorForBodyEl.textContent = newColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
